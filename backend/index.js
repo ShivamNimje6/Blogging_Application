a17883f48cb895a62ecfa95ce0c37736eb390893
ShivamNimje6 <shivamnimje6@gmail.com>
2024-07-08 23:35:25 +0530

    updated index.js

--- backend/index.js
+++ backend/index.js
@@ -32,7 +32,7 @@ app.use("/images", express.static(path.join(__dirname, "/images")));
 //hellooooo
 // / Updated CORS setup
 const corsOptions = {
-  origin: "https://blogapp2-kd9o44a7d-shivam-nimjes-projects.vercel.app ",
+  origin: "https://blogapp3-pqq0p5iua-shivam-nimjes-projects.vercel.app",
   credentials: true,
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   allowedHeaders: ["Content-Type", "Authorization"],
